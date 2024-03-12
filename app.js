2bc0cbc184ba93f0c08028b8b9a263f6c785ce69
Dev Harsh <devharsh466@gmail.com>
2024-03-13 00:44:53 +0530

    creating routes

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 
 const PORT = process.env.port || 3000;
 
+const product_routes = require("./routes/products");
+
 app.get("/" , (req,res)=>{
 res.send("hi am live");
 });
+
+app.use("/api/products" , product_routes);
 
 const start = async () =>{
     try{
