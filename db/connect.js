a2dd6b950dfa974ae1c6d05ad2dac040c354e7e2
Dev Harsh <devharsh466@gmail.com>
2024-03-16 12:56:04 +0530

    deploy edit

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = (uri) => {
-  return mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  return mongoose.connect(uri);
 };
 
 module.exports = connectDB;
